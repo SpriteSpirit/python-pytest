2c6c5a1d710a4b52b174c11d474f7b678fba511a
angelinakhalueva <sprite.spirit.t.me@gmail.com>
2023-12-10 23:43:10 +0300

    make 100% coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,10 +2,15 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2], -1, "test") == "test"
+    assert arrs.get([1, 2, 3, 4, 5, 6], 23, "test") == "test"
 
 
-def test_slice():
+def test_my_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2], -1, 2) == [2]
+    assert arrs.my_slice([1, 2], None, 2) == [1, 2]
+    assert arrs.my_slice([], 0, 2) == []
